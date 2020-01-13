5bea29d301a8e871517453c9a4965156aed49917
AndreasHasNoHope <tsimtsiris1999@hotmail.com>
2020-01-13 21:33:32 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { ProductCreateComponent } from './components/product-create/product-create.component';
+import { ProductUpdateComponent } from './components/product-update/product-update.component';
+
+const routes = [
+  {
+    path: "",
+    component: DashboardComponent
+  },
+  {
+    path: "products",
+    children: [
+      {
+      path: "",
+      component: ProductsComponent
+      },
+      {
+        path: "create",
+        component: ProductCreateComponent,
+      },
+      {
+        path: "update/:productId",
+        component: ProductUpdateComponent
+      }
+
+    ]
+  },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ProductsComponent,
+    ProductCreateComponent,
+    ProductUpdateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CommonModule, //of ngif to works
+    FormsModule, //ng-model so that it works
+    RouterModule.forRoot(routes),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IProduct} from "../../interfaces/IProduct";
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  public products: IProduct[] = [];
+  public loading:boolean = false;
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+   this.getProducts()
+  }
+  public getProducts() {
+    this.loading = true;
+    this.http.get<IProduct[]>(environment.apiUrl + "/products").subscribe(response => {
+      this.products = response;
+      this.loading = false;
+    });
+  }
+  public deleteProduct(id){
+    this.http.delete(environment.apiUrl + "/products/" + id)
+      .subscribe(_ => {this.getProducts()});
   }
 
 }
+
